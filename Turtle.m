fe6a337a1d7eecb08caec92a2b1542a6807487a6
Krzysztof G <geletakrzysztof1997@gmail.com>
2019-03-03 20:11:44 +0100

    working turtle

--- Turtle.m
+++ Turtle.m
@@ -1,19 +1,75 @@
-classdef Turtle
+classdef Turtle < handle
     % LOGO turtle
     
     properties
         x;
         y;
+        angle;
+        
         step_size;
         angle_increment;
     end
     
     methods
-        function obj = Turtle(x0, y0, step_size, angle_increment)
-            obj.x = x0;
-            obj.y = y0;
-            obj.step_size = step_size;
-            obj.angle_increment = angle_increment;
+        function this = Turtle(x0, y0, angle, step_size, angle_increment)
+            this.x = x0;
+            this.y = y0;
+            this.angle = angle;
+            this.step_size = step_size;
+            this.angle_increment = angle_increment;
+        end
+        
+        function newPicture = draw(this, picture, string)
+            newPicture = picture;
+            for i = 1:length(string)
+                % for testing:
+                %fprintf("X = %d Y = %d angle = %d\n", this.x, this.y, this.angle);
+                switch string(i)
+                    case 'F'
+                        newPicture = this.moveDraw(newPicture);
+                    case 'f'
+                        this.move();
+                    case '+'
+                        this.turnRight();
+                    case '-'
+                        this.turnLeft();
+                    otherwise
+                        disp('wrong character!');                        
+                end
+            end
+        end
+        
+    end
+    
+    methods (Access = private)
+        function newPicture = moveDraw(this, picture)
+            newPicture = picture;
+            
+            newX = round(this.x + this.step_size * cosd(this.angle));
+            newY = round(this.y + this.step_size * sind(this.angle));
+    
+            nPoints = max(abs(diff([newX, this.x])), abs(diff([newY, this.y])))+1;
+            
+            rIndex = round(linspace(this.x, newX, nPoints));
+            cIndex = round(linspace(this.y, newY, nPoints));
+            index = sub2ind(size(newPicture), rIndex, cIndex);
+            newPicture(index) = 0;
+            
+            this.x = newX;
+            this.y = newY;            
+        end
+        
+        function move(this)
+            this.x = round(this.x + this.step_size * cosd(this.angle));
+            this.y = round(this.y + this.step_size * sind(this.angle));
+        end
+        
+        function turnRight(this)
+            this.angle = this.angle + this.angle_increment;
+        end
+        
+        function turnLeft(this)
+            this.angle = this.angle - this.angle_increment;
         end
     end
     
--- main.m
+++ main.m
@@ -4,7 +4,38 @@ clc;
 
 % quadratic Koch island:
 productions = containers.Map({'F'}, {'F+F-F-FF+F+F-F'});
+
 axiom = 'F+F+F+F';
 
-test = generate_successor(axiom, productions);
-test2 = generate_successor(test, productions);
+string1 = generate_successor(axiom, productions);
+string2 = generate_successor(string1, productions);
+string3 = generate_successor(string2, productions);
+
+
+
+myTurtle = Turtle(170, 180, 0, 150, 90);
+picture = ones(512, 512) * 255;
+
+%% n = 0
+
+figure;
+imshow(myTurtle.draw(picture, axiom));
+
+%% n = 1
+
+myTurtle.step_size = 20;
+figure;
+imshow(myTurtle.draw(picture, string1));
+
+
+%% n = 2
+
+myTurtle.step_size = 5;
+figure;
+imshow(myTurtle.draw(picture, string2));
+
+%% n = 3
+
+myTurtle.step_size = 2;
+figure;
+imshow(myTurtle.draw(picture, string3));
