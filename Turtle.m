8c71ca3a47d677bf5a393b12c08bd5500df46e86
Krzysztof G <geletakrzysztof1997@gmail.com>
2019-03-04 14:27:03 +0100

    turtle with automatic size for picture

--- Turtle.m
+++ Turtle.m
@@ -1,5 +1,5 @@
 classdef Turtle < handle
-    % LOGO turtle
+    % turtle
     
     properties
         x;
@@ -8,25 +8,26 @@ classdef Turtle < handle
         
         step_size;
         angle_increment;
+        offset;
     end
     
     methods
-        function this = Turtle(x0, y0, angle, step_size, angle_increment)
-            this.x = x0;
-            this.y = y0;
-            this.angle = angle;
+        function this = Turtle(step_size, angle_increment)
+            this.angle = 0;
             this.step_size = step_size;
             this.angle_increment = angle_increment;
+            
+            this.offset = 10;
         end
         
-        function newPicture = draw(this, picture, string)
-            newPicture = picture;
+        function picture = draw(this, string)
+            picture = ones(this.calculate(string)) * 255;
             for i = 1:length(string)
-                % for testing:
+                %for testing:
                 %fprintf("X = %d Y = %d angle = %d\n", this.x, this.y, this.angle);
                 switch string(i)
                     case 'F'
-                        newPicture = this.moveDraw(newPicture);
+                        picture = this.moveDraw(picture);
                     case 'f'
                         this.move();
                     case '+'
@@ -71,6 +72,51 @@ classdef Turtle < handle
         function turnLeft(this)
             this.angle = this.angle - this.angle_increment;
         end
+        
+        function size = calculate(this, string)
+            currentX = 0;
+            currentY = 0;
+            angle = 0;
+            
+            minX = 0;
+            minY = 0;
+            maxX = 0;
+            maxY = 0;
+            
+            for i = 1:length(string)
+                switch string(i)
+                    case 'F'
+                        currentX = round(currentX + this.step_size * cosd(angle));
+                        currentY = round(currentY + this.step_size * sind(angle));
+                    case 'f'
+                        currentX = round(currentX + this.step_size * cosd(angle));
+                        currentY = round(currentY + this.step_size * sind(angle));
+                    case '+'
+                        angle = angle + this.angle_increment;
+                    case '-'
+                        angle = angle - this.angle_increment;
+                    otherwise
+                        disp('wrong character!');                        
+                end
+                if(currentX < minX)
+                    minX = currentX;
+                end
+                if(currentX > maxX)
+                    maxX = currentX;
+                end
+                if(currentY < minY)
+                    minY = currentY;
+                end
+                if(currentY > maxY)
+                    maxY = currentY;
+                end
+            end
+            this.x = abs(minX) + this.offset;
+            this.y = abs(minY) + this.offset;
+            
+            size = [(maxX - minX) + 2 * this.offset, (maxY - minY) + 2 * this.offset];
+        end
+        
     end
     
 end
--- main.m
+++ main.m
@@ -2,40 +2,34 @@ close all;
 clearvars;
 clc;
 
-% quadratic Koch island:
-productions = containers.Map({'F'}, {'F+F-F-FF+F+F-F'});
+% examples of Koch constructions:
 
+%%
+
+productions = containers.Map({'F'}, {'F+F-F-FF+F+F-F'});
 axiom = 'F+F+F+F';
 
 string1 = generate_successor(axiom, productions);
 string2 = generate_successor(string1, productions);
 string3 = generate_successor(string2, productions);
 
-
-
-myTurtle = Turtle(170, 180, 0, 150, 90);
-picture = ones(512, 512) * 255;
-
-%% n = 0
-
 figure;
-imshow(myTurtle.draw(picture, axiom));
+myTurtle = Turtle(3, 90);
+imshow(myTurtle.draw(string3));
 
-%% n = 1
+%% 
+productions = containers.Map({'F', 'f'}, {'F-f+FF-F-FF-Ff-FF+f-FF+F+FF+Ff+FFF', 'ffffff'});
+axiom = 'F-F-F-F';
 
-myTurtle.step_size = 20;
+string1 = generate_successor(axiom, productions);
+string2 = generate_successor(string1, productions);
+string3 = generate_successor(string2, productions);
+
+myTurtle = Turtle(5, 90);
 figure;
-imshow(myTurtle.draw(picture, string1));
+imshow(myTurtle.draw(string2));
 
 
-%% n = 2
 
-myTurtle.step_size = 5;
-figure;
-imshow(myTurtle.draw(picture, string2));
 
-%% n = 3
 
-myTurtle.step_size = 2;
-figure;
-imshow(myTurtle.draw(picture, string3));
